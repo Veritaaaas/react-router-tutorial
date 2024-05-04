b57615616f68a695016c5a3f2259d28aa3db0422
Rodney James <124419269+Veritaaaas@users.noreply.github.com>
2024-05-04 14:36:53 +0000

    revised the app component

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,16 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      
-    </>
-  )
-}
+    <div>
+      <h1>Hello from the main page of the app!</h1>
+      <p>Here are some examples of links to other pages</p>
+      <nav>
+        <ul>
+          <li>
+            <a href="profile">Profile page</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
-export default App
+export default App;
+
